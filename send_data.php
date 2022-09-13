9d333854987d1aa255db4aee56fb168093c77edd
HawraaYounes <hawraayounes0@gmail.com>
2022-09-13 23:29:18 +0300

    Finish send_data.php section

--- send_data.php
+++ send_data.php
@@ -7,7 +7,7 @@ $phone = $_POST["phone"];
 $message = $_POST["message"];
 
 $query = $mysqli->prepare("INSERT INTO users(fullname, email, phone, message) VALUE (?, ?, ?, ?)");
-$query->bind_param("ss", $name, $author);
+$query->bind_param("ssss",$fullname, $email, $phone, $message );
 $query->execute();
 
 $response = [];
